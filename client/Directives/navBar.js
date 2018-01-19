b2d8dde0a1a5c7cdb178d0b603f7c19414c77134
tejaswaroopgit <30846434+tejaswaroopgit@users.noreply.github.com>
2018-01-19 15:01:05 -0500

    Updated
    Changes

--- client/Directives/navBar.js
+++ client/Directives/navBar.js
@@ -1,6 +1,6 @@
 myApp.directive('navBar', function () {
     return {
         template: '' +
-        '<a href="#/" class="btn btn-primary" style="margin:10px 10px 10px 110px;border-radius:15px">home</a>'
+        '<a href="#/" class="btn btn-default" style="margin:10px 10px 10px 110px;border-radius:15px"><img src="../images/h1.jpg" height="75" width="75" /></a>'
     }
 })
